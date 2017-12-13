1fff3f299b1ad3d3e792150d02cb8a2b5ca387f6
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2017-12-13 13:18:47 -0200

    added main game files

---
+++ idleS.asm
@@ -0,0 +1,494 @@
+.data
+    # ResoluÃ§Ã£o da tela
+Largura:  .word 512
+Altura:   .word 512
+
+args: .word 0,0
+menu_0: .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,5,5,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,5,5,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,5,5,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,5,5,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,5,5,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    
+    0,0,0,0,5,5,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,5,5,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,5,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,7,1,1,1,7,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,0,0,0,1,1,1,7,7,7,1,7,7,7,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
+    
+    1,1,1,0,1,1,1,7,7,7,7,7,7,7,7,7,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
+    1,1,1,0,1,1,1,1,7,7,7,7,7,7,7,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,1,0,1,1,1,1,1,7,7,7,7,7,1,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,1,0,1,1,1,1,1,1,7,7,7,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,0,0,0,1,1,1,1,1,1,7,1,1,1,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,0,0,1,0,0,0,1,0,0,1,0,0,1,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,0,1,1,0,1,0,1,0,1,1,0,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    
+    1,1,0,1,1,0,0,0,1,0,0,1,0,0,1,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,0,1,1,0,1,0,1,0,1,1,0,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,0,1,1,0,1,0,1,0,1,1,0,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,
+    1,1,0,0,1,0,1,0,1,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    
+menu_1: .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,5,0,0,0,0,5,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,5,5,0,0,5,5,0,0,0,5,5,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,5,5,5,0,5,5,5,0,0,0,5,5,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,5,5,0,0,5,5,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,5,5,0,0,5,5,5,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,5,5,0,0,0,5,5,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,5,0,0,0,0,5,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    
+    0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
+    0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
+    0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,
+    0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,
+    0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,
+    0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,
+    0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,
+    0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,
+    
+    0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
+    0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
+    0,0,0,0,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,0,0,0,0,
+    0,0,0,0,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,0,0,0,0,
+    0,0,0,0,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,0,0,0,0,
+    0,0,0,0,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,0,0,0,0,
+    0,0,0,0,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,0,0,0,0,
+    0,0,0,0,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,0,0,0,0,
+    
+    0,0,0,0,0,0,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,0,0,0,0,0,0,
+    0,0,0,0,0,0,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,3,3,4,4,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
+    
+
+  
+menu_2: .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    
+    0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    
+    0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,0,0,0,
+    
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,0,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,
+    0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,
+    0,0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,1,1,1,1,6,6,6,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,
+    
+    0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,
+    0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,
+    0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    
+ceu1:   .word 0x000000 #0xfef1ba  
+ceu2: .word 0xe6e6e6 #0xffefaa
+ceu3: .word 0xb3b3b3 #0xfceaa0
+sol1: .word 0xcc8800 #0xfaf1c8
+sol2: .word 0x663300 #marrom
+sol3: .word 0xe6ffff #azul fumaÃ§a
+sol4: .word 0xadad85 #cinza claro
+sol5: .word 0xff1a1a #vermelho
+
+opening:    .asciiz     "Congratulations, you are the proud new owner of a small Coffe Shop!! \n Now go make some money... "
+msg:    .asciiz     "current money: "
+actionMsg:    .asciiz     "(press 0 to brew coffe) (+10$)\n"
+msg2:    .asciiz     "current profit per day: "
+msg3:    .asciiz     "Buy coffe maker? (+1$ per day) [press 1] Price: "
+msg4:    .asciiz     "Hire workers (+10$ per day) [press 2] Price: "
+msg5:    .asciiz     "Fancy Cappucino Machines (+5$ per day) [press 3] Price: "
+msgr:    .asciiz     "Research the secrets of Coffe (unlock more upgrades) [press 7] Price: "
+msgr1:    .asciiz     "[[[ Learn the basics of coffe making: Expreso, Cappucino, Milk,... ]]]"
+msgb:    .asciiz     "Invest in yout business (Increase current daily profits) [press 8] Price: "
+msgb1:    .asciiz     "[[[ Buy Ads in local journal *2x DAILY PROFITS* ]]]"
+msgb2:    .asciiz     "[[[ Theme Investment: Maid Cafe *2x DAILY PROFITS + Current workers become twice as effective* ]]]"
+newline: .asciiz "\n"
+newblock: .asciiz "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+clear:  .byte   0x1B,0x5B,0x33,0x3B,0x4A,0x1B,0x5B,0x48,0x1B,0x5B,0x32,0x4A
+cm: .word 20
+hw: .word 180
+cmN: .word 0
+fcm: .word 80
+fcmN: .word 0
+hwN: .word 0
+rp: .word 1000
+bp: .word 1000
+rl: .word 1
+bl: .word 1
+eclear:
+        .byte 0x00
+
+    .text
+    .globl main
+main:
+    li $s0, 30000
+    li $s1, 0
+    la $a0, newblock
+    li      $v0,4
+    syscall
+     la $a0, opening           # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+     li      $t5,90000
+     
+op_delay:
+    subiu   $t5,$t5,1
+    bnez    $t5,op_delay    
+main_loop:
+    add $s0, $s0, $s1
+screen:
+    la $a2,128($gp)
+    jal DesenhaBloco
+    # clear the screen
+    #la      $a0,clear
+    la $a0, newblock
+    li      $v0,4
+    syscall
+     la $a0, msg           # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall    
+     move $a0, $s0
+     li $v0, 1
+     syscall
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall    
+     la $a0, msg2         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall    
+     move $a0, $s1
+     li $v0, 1
+     syscall
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+     
+     la $a0, actionMsg         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall 
+
+     
+     lw $t5, cm
+     blt $s0, $t5, checkIfAction  #  if less or equals to 20
+     la $a0, msg3           
+     li $v0, 4       
+     syscall      
+     move $a0, $t5
+     li $v0, 1
+     syscall
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+     
+     lw $t7, rl
+     blt $t7, 2, afterFancyCoffe
+     lw $t5, fcm
+     blt $s0, $t5, endResearch  #  if less or equals to 80
+     la $a0, msg5           
+     li $v0, 4       
+     syscall      
+     move $a0, $t5
+     li $v0, 1
+     syscall
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+     lw $t7, rl
+    afterFancyCoffe:
+
+     lw $t5, hw
+     blt $s0, $t5, startResearch #  if less or equals to 150
+     la $a0, msg4           
+     li $v0, 4       
+     syscall      
+     move $a0, $t5
+     li $v0, 1
+     syscall
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+    la $a0, newline         # load str1 address into $a0
+    li $v0, 4              # load I/O code for string output
+    syscall
+     
+startResearch:
+     lw $t5, rp
+     blt $s0, $t5, endResearch  #  if less or equals to price of research
+     lw $t7, rl
+     
+     #  switch-case for research level rl
+     bne $t7, 1, C2
+     j C1BODY
+C1BODY:
+
+     
+     la $a0, msgr1         
+     li $v0, 4       
+     syscall      
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+     j researchMessage
+C2:  bne $t7, 2, C3
+     j C2BODY
+C2BODY:
+
+C3:
+     
+
+researchMessage:
+    lw $t7, rl
+    bgt $t7, 1, endResearch # if above current maximum research level
+     la $a0, msgr           
+     li $v0, 4       
+     syscall      
+     move $a0, $t5
+     li $v0, 1
+     syscall
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+endResearch:
+
+
+lw $t5, bp
+blt $s0, $t5, endInvestment #  if less or equals to price of investment     
+     lw $t7, bl
+     bne $t7, 1, C2B
+     j C1BODYB
+C1BODYB:
+     la $a0, msgb1         
+     li $v0, 4       
+     syscall      
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+     j investmentMessage
+C2B:
+    bne $t7, 2, C3B
+    j C2BODYB
+C2BODYB:
+     la $a0, msgb2         
+     li $v0, 4       
+     syscall      
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+     j investmentMessage
+
+C3B:
+    
+investmentMessage:
+lw $t7, bl
+bgt $t7, 2, endInvestment# if above current maximum investment level
+     la $a0, msgb           
+     li $v0, 4       
+     syscall      
+     move $a0, $t5
+     li $v0, 1
+     syscall
+     la $a0, newline         # load str1 address into $a0
+     li $v0, 4              # load I/O code for string output
+     syscall
+endInvestment:
+     
+checkIfAction:
+     lw $t5, 0xFFFF0004     # check to see which key has been pressed
+     bnez $t5, finishedCoffe # check to see if key is 0
+     
+     
+    # wait a bit
+wait:
+    li      $t6,20000
+main_delay:
+    lw $t5, 0xFFFF0004      # check to see which key has been pressed
+    beq $t5, 0x00000030, brewCoffe # 0 pressed  
+    beq $t5, 0x00000031, buyCoffeMaker # 1 pressed
+    beq $t5, 0x00000032, hireWorker # 2 pressed
+    beq $t5, 0x00000033, buyFancyMachines # 3 pressed            
+    beq $t5,  0x00000037, researchUpgrade
+    beq $t5,  0x00000038, businessInvestment
+afterAction: 
+    subiu   $t6,$t6,1
+    bgtz    $t6,main_delay
+    j       main_loop
+brewCoffe:
+   addi $s0, $s0, 1
+   j screen
+finishedCoffe:
+   move $t5, $zero
+   sb $t5, 0xFFFF0004
+   j afterAction
+buyCoffeMaker:   
+   lw $t5, cm
+   lw $t7, cmN
+   blt $s0, $t5, finishedCoffe  #  if less or equals to 200
+   sub $s0, $s0, $t5
+   mul $t5, $t5, 2
+   addi $t7, $t7, 1
+   addi $s1, $s1, 1
+   sw $t7, cmN
+   sw $t5, cm
+   j screen
+buyFancyMachines:   
+   lw $t5, fcm
+   lw $t7, fcmN
+   blt $s0, $t5, finishedCoffe  #  if less or equals to 200
+   sub $s0, $s0, $t5
+   mul $t5, $t5, 2
+   addi $t7, $t7, 1
+   addi $s1, $s1, 5
+   sw $t7, fcmN
+   sw $t5, fcm
+   j screen
+hireWorker:   
+   lw $t5, hw
+   lw $t7, hwN
+   blt $s0, $t5, finishedCoffe  #  if less or equals to 200
+   sub $s0, $s0, $t5
+   mul $t5, $t5, 2
+   addi $s1, $s1, 10
+   addi $t7, $t7, 1
+   sw $t7, hwN
+   sw $t5, hw
+   j screen
+researchUpgrade:
+   lw $t5, rp
+   lw $t7, rl
+   blt $s0, $t5, finishedCoffe  #if less or equal to price of research
+   sub $s0, $s0, $t5
+   mul $t5, $t5, 5
+   addi $t7, $t7, 1
+   sw $t7, rl
+   sw $t5, rp
+   j screen
+businessInvestment:
+   lw $t5, bp
+   lw $t7, bl
+   blt $s0, $t5, finishedCoffe  #  if less or equals to price of investment
+   sub $s0, $s0, $t5
+   mul $s1, $s1, 2
+   mul $t5, $t5, 5
+   bne $t7, 2, bicontinue
+   lw $t7, hwN
+   mul $t7, $t7, 10
+   add $s1, $s1, $t7
+
+bicontinue:
+   addi $t7, $t7, 1
+   sw $t7, bl
+   sw $t5, bp
+   j screen
+
+#################################################################
+# DesenhaBloco              #
+#################################################################
+  
+DesenhaBloco:
+  bgt $s1, 10, mediumCoffeShop
+  la $t0,menu_2
+  j readyCoffeShop
+  mediumCoffeShop:
+  bgt $s1, 100, smallCoffeShop
+  la $t0,menu_1
+   j readyCoffeShop
+  smallCoffeShop:
+  la $t0,menu_0
+  readyCoffeShop:
+  li $t1,-1
+  li $t3,0
+  la $t4,($ra)
+printBloco:
+    lb $a0,0($t0)
+    jal RetornaCor
+    move $a1,$v0
+    addi $t0,$t0,1
+    addi $t1,$t1,1
+    addi $t3,$t3,1
+    bne $t1,32,tagBloco
+    add $a2,$a2,512
+    li $t1,0
+tagBloco: 
+    mul $t2,$t1,4
+    add $t2,$a2,$t2
+    sw  $a1,0($t2)
+    bne $t3,1024,printBloco
+    la $ra,($t4)
+    jr $ra
+
+#################################################################
+# RetornaCor              #
+# $a0 -> nÃºmero da cor            #
+# retorna $v0 -> cÃ³digo da cor          #
+#################################################################
+
+RetornaCor:
+TestaCor0:
+  bne $a0,0,TestaCor1
+  lw $v0,ceu1
+  j Fim
+TestaCor1:
+  bne $a0,1,TestaCor2
+  lw $v0,ceu2
+  j Fim
+TestaCor2:
+  bne $a0,2,TestaCor3
+  lw $v0,ceu3
+  j Fim
+TestaCor3:
+  bne $a0,3,TestaCor4
+  lw $v0,sol1
+  j Fim
+TestaCor4:
+  bne $a0,4,TestaCor5
+  lw $v0,sol2
+  j Fim
+TestaCor5:
+  bne $a0,5,TestaCor6
+  lw $v0,sol3
+  j Fim
+TestaCor6:
+  bne $a0,7,TestaCor7
+  lw $v0,sol5
+  j Fim
+TestaCor7:
+  bne $a0,6,Fim
+  lw $v0,sol4
+  j Fim
+  
+Fim:
+  jr $ra      # retorna $v0
